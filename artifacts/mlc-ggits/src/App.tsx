ebcf3e2c46d829bc2f8c5cb66368208a0462e15d
revanshsharma <sharmarevansh4@gmail.com>
2026-05-01 15:11:10 +0530

    Update App.tsx

--- artifacts/mlc-ggits/src/App.tsx
+++ artifacts/mlc-ggits/src/App.tsx
@@ -623,9 +623,9 @@ function Home() {
             </div>
 
             <h1 className="text-4xl sm:text-5xl md:text-7xl lg:text-8xl font-extrabold tracking-tight mb-4 leading-tight">
-              Microsoft Campus <br />
+              Microsoft Learm <br />
               <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#0078D4] via-[#00D4FF] to-[#00FF41]">
-                Club
+                Community
               </span>
             </h1>
 
